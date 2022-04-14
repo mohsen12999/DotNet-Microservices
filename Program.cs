133083667a7e0a9c82373d853b49d95cb84ff6f0
mohsen1299@gmail.com <mohsen1299@gmail.com>
2022-04-15 01:38:01 +0430

    register platformrepo in services

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(opt =>
  opt.UseInMemoryDatabase("InMem"));
+
+builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
  
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle

cf8998d6efb0051e6b871da4439636f78bcf360b
mohsen1299@gmail.com <mohsen1299@gmail.com>
2022-04-27 01:07:34 +0430

    add test controller for our command project

---
+++ CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controller;
+
+[Route("api/c/[controller]")]
+[ApiController]
+public class PlatformsController : ControllerBase
+{
+    public PlatformsController()
+    {
+        
+    }
+
+    [HttpPost]
+    public ActionResult TestInboundConnection()
+    {
+        Console.WriteLine("--> Inbound Post # Command Service");
+        
+        return Ok("Inbound test of from Platforms Controller");
+    }
+}

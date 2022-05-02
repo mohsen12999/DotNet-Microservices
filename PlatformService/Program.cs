9e5e02daf9f5b69ca6005efe745c71cdd3d93c32
mohsen1299@gmail.com <mohsen1299@gmail.com>
2022-05-03 02:27:47 +0430

    write command port number in output

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -29,6 +29,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+Console.WriteLine($"--> Command Service Endpoint {builder.Configuration["CommandService"]}");
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();

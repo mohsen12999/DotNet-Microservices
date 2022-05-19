d6709cfd6423f7a5df84cf4aa683781e8474b161
mohsen1299@gmail.com <mohsen1299@gmail.com>
2022-05-20 01:15:49 +0430

    add interface for command project repo

---
+++ CommandsService/Data/ICommandRepo.cs
@@ -0,0 +1,18 @@
+using CommandsService.Models;
+
+namespace CommandsService.Data;
+
+public interface ICommandRepo
+{
+    bool SaveChanges();
+
+    // Platforms
+    IEnumerable<Platform> GetAllPlatform();
+    void CreatePlatform(Platform platform);
+    bool PlatformExist(int platformId);
+
+    // Commands
+    IEnumerable<Command> GetCommandsForPlatform(int platformId);
+    Command GetCommand(int platformId, int commandId);
+    void CreateCommand(int platformId, Command command);
+}

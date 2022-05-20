65b52334e4e5e550135e3db805be5145f0555bb4
mohsen1299@gmail.com <mohsen1299@gmail.com>
2022-05-20 23:01:12 +0430

    add get all platform to command controller

--- CommandsService/Controllers/PlatformsController.cs
+++ CommandsService/Controllers/PlatformsController.cs
@@ -1,3 +1,6 @@
+using AutoMapper;
+using CommandsService.Data;
+using CommandsService.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CommandsService.Controller;
@@ -6,9 +9,23 @@ namespace CommandsService.Controller;
 [ApiController]
 public class PlatformsController : ControllerBase
 {
-    public PlatformsController()
+    private readonly ICommandRepo _repository;
+    private readonly IMapper _mapper;
+
+    public PlatformsController(ICommandRepo repository, IMapper mapper)
     {
-        
+        _repository = repository;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<PlatformReadDto>> GetPlatform()
+    {
+        Console.WriteLine("-> Getting platforms from  Commands Service.");
+
+        var platformItems = _repository.GetAllPlatform();
+
+        return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItems));
     }
 
     [HttpPost]
